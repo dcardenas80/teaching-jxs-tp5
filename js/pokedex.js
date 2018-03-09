25d24350639d732ca4bdd10d3b961ad2e70ac6de
Diego <dym2312@hotmail.com>
2018-03-09 21:56:32 +0100

    Watch et service pour faire parler les des controlateurs

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,73 +1,73 @@
-var pokeApp = angular.module('pokedex', ['ngResource']);
-pokeApp.controller('pokemonSearch', ['$scope', '$log', pokemonSearch]);
-pokeApp.controller('pokemonSearchAPI', ['$scope','Pokemon', pokemonSearchAPI]);
+var pokeApp = angular.module('pokedex', ['ngResource']).service('pokeJoin',pokeJoin);
+pokeApp.controller('pokemonSearch', ['$scope', '$log', '$http','getPokemonList','pokeJoin',pokemonSearch]);
+pokeApp.controller('pokemonSearchAPI', ['$scope','getPokemon', 'pokeJoin' , pokemonSearchAPI]);
+
 
 // With this you can inject POKEAPI url wherever you want
-pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+pokeApp.constant('POKEAPI', 'https://pokeapi.co');
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
-pokeApp.factory('Pokemon', function($resource) {
+pokeApp.factory('getPokemon', function($resource, POKEAPI) {
     return $resource("https://pokeapi.co/api/v2/pokemon/:id/", {}, {
-            query: { method: "GET", isArray: true },
-            create: { method: "POST"},
-            get: { method: "GET"},
-            remove: { method: "DELETE"},
-            update: { method: "PUT"} });
-         // Note the full endpoint address
+        get: { method: "GET"}});
+     // Note the full endpoint address
 });
-  
-function pokemonSearchAPI($scope,Pokemon){
-    
-    $scope.poke = Pokemon.get({ id: 24});
-    $scope.poke.$promise.then(function (result) {
-        $scope.idPokedex = result.id;
-        $scope.name = result.name;
-        $scope.abilities = result.abilities;
+pokeApp.factory('getPokemonList', ['$resource', 'POKEAPI', function($resource, POKEAPI) {
+    return $resource(POKEAPI + "/api/v2/pokemon/?limit=802&offset=0",{},{
+        get: {method: "GET"},
     });
+}]);
 
-   
-    $scope.getPokemon = function() {
-        $scope.idPokedex = poke.id;
-        $scope.name = poke.name;
-        $scope.abilities = poke.abilities;
-    };
-}
- 
-function pokemonSearch($scope,$log) {
+/**
+ * Controlleur charge de la recherche de pokemons
+ * 
+ * */  
+
+ /**
+ * Controlleur charge de la recherche de pokemons
+ * 
+ * */ 
+function pokemonSearch($scope,$log, $http,getPokemonList,pokeJoin) {
    
     $scope.pokemons = [
-      {id:'1', name:'Pikachu'},
-      {id:'2', name:'Charmander'},
-      {id:'3', name:'Squirtle'},
-      {id:'4', name:'Bulbasaur'},
-      {id:'5', name:'jigglypuff'}
-    ];
+      ];
     $scope.$log = $log;
-  
-    $scope.searchByFilter = function(idPokemon) {
-        var index = $scope.pokemons.indexOf(idPokemon);
-        alert("Search Result:"+ $scope.pokemons );
-      };
+    getPokemonList.get().$promise.then(function (result) {
+        $scope.pokemons = result.results;
+        $scope.pokemons.forEach(function(e){
+            e.id = e.url.replace('https://pokeapi.co/api/v2/pokemon/','').replace('/','')
+          });
+    });
+    console.log($scope.pokemons);
+
+    $scope.searchPok=function(pokemon) {
+     
+            pokeJoin.addPokemon(pokemon.id);
+        
+    }
+
 }  
 
-pokeApp.factory('pokeJoin', function() {
-    var savedPoke = {
-        idNumber : null,
-        pokeName : ""
 
+  function pokeJoin(){
+    this.addPokemon = function(id) {
+        this.id = id;
     };
-     function addPokemon(id, name){
-        savedPoke.idNumber = id;
-        savedPoke.pokeName = name;
-     }
-     function getPokemon(){
-        return savedPoke;
-     }
-    // factory function body that constructs shinyNewServiceInstance
-    return  {
-        addPokemon: addPokemon,
-        getPokemon: getPokemon
-      };
-  }); 
+    this.getPokemon = function() {
+        return this.id;
+    };
+};
+
+function pokemonSearchAPI($scope,getPokemon,pokeJoin){
+    $scope.service = pokeJoin;
+  
+    console.log(pokeJoin.getPokemon());
+    $scope.getItem = function() {
+        $scope.poke = getPokemon.get({id: pokeJoin.getPokemon()})
+    
+    };
+    $scope.$watch('service.getPokemon()', $scope.getItem);
+
+}
